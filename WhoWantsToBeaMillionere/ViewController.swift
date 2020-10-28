98467b27355cf830f01e037f63c4d8355cb72be5
Sergei Morugin <cmorugin@gmail.com>
2020-10-28 19:59:04 +0300

    init

---
+++ WhoWantsToBeaMillionere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhoWantsToBeaMillionere
+//
+//  Created by Matthew on 28.10.2020.
+//  Copyright © 2020 Ostagram Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
